52d992e08e757bff22e8d54717458760fbe15bce
Tina <florentinapatcas@gmail.com>
2020-10-12 21:19:27 +0300

    first form idea

---
+++ src/org/fasttrackit/Main.java
@@ -0,0 +1,9 @@
+package org.fasttrackit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HelloGitHub");
+    }
+
+
+}
---
+++ src/org/fasttrackit/test2/Main.java
@@ -0,0 +1,8 @@
+package org.fasttrackit.test2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HelloGitHub");
+    }
+}
